875ed2bb0d3444c7b205c7acf3050807082b74b2
joe-hollinshead-kidsloop <joe.hollinshead@kidsloop.live>
2022-05-20 15:31:23 +0100

    Placeholder unit test

---
+++ test/unit/handler_test.go
@@ -0,0 +1,14 @@
+package handler_test
+
+import (
+	"testing"
+	"time"
+)
+
+func TestTest(t *testing.T) {
+	t.Log("test 1")
+	time.Sleep(15000)
+	t.Log("test 2")
+	time.Sleep(15000)
+	t.Log("test 3")
+}
