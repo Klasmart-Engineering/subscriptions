cfc73ff21f66a28a659017ec696fab7d88489407
joe-hollinshead-kidsloop <joe.hollinshead@kidsloop.live>
2022-06-24 12:30:02 +0100

    Temporarily add api key to test on api factory

---
+++ database/migrations/6_temp_api_key.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO api_key (owner, api_key) VALUES ('Temporary', 'Hr4TxdpfhODrcMwI');
+INSERT INTO api_key_permission (owner, permission) VALUES
+    ('Temporary', 'create-subscription'),
+    ('Temporary', 'get-subscription');
