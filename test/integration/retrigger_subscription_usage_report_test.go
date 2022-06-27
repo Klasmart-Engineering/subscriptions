e1b96df9ccd0319df1ed9181bf5f9a07e0b30a07
joe-hollinshead-kidsloop <joe.hollinshead@kidsloop.live>
2022-06-27 17:10:22 +0100

    Fix tests

--- test/integration/retrigger_subscription_usage_report_test.go
+++ test/integration/retrigger_subscription_usage_report_test.go
@@ -3,10 +3,8 @@ package integration_test
 import (
 	"context"
 	"encoding/json"
-	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 	"net/http"
-	"strings"
 	"subscriptions/src/api"
 	"subscriptions/test/integration/helper"
 	"testing"
@@ -30,21 +28,11 @@ func TestPatchSubscriptionUsageWithoutValidSubscriptionReturns404(t *testing.T) 
 func TestPatchSubscriptionUsageWithValidSubscriptionReturns200(t *testing.T) {
 	helper.ResetDatabase()
 	helper.WaitForHealthcheck(t)
-	helper.RunTestSetupScript("api-keys.sql")
+	helper.RunTestSetupScript("existing-usage-report.sql")
 
-	response, err := apiClient.PostSubscriptions(context.Background(), api.PostSubscriptionsJSONRequestBody{
-		AccountId: uuid.MustParse("be372162-c0a0-4903-a9e1-a0b372bb1de9"),
-	}, func(ctx context.Context, req *http.Request) error {
-		req.Header.Add("X-Api-Key", "Bearer valid-key-with-permission")
-		return nil
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
+	subscriptionId := "c015ce36-76df-4f3f-9352-5daea102d150"
 
-	subscriptionId := strings.Replace(response.Header.Get("Location"), "/subscriptions/", "", 1)
-
-	resp, err := apiClient.PatchSubscriptionsSubscriptionIdUsageReportsUsageReportId(context.Background(), subscriptionId, "d456d6cd-df69-40aa-b268-58e1234e3225", func(ctx context.Context, req *http.Request) error {
+	resp, err := apiClient.PatchSubscriptionsSubscriptionIdUsageReportsUsageReportId(context.Background(), subscriptionId, "7a0e62ee-2685-4220-b3b6-3dd0e0c059b9", func(ctx context.Context, req *http.Request) error {
 		req.Header.Add("X-Api-Key", "Bearer valid-key-no-permission")
 		req.Header.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdWItYmxhYmxhIiwibmFtZSI6IlNvbWVib2R5IiwiaWF0IjoxMjM0NTYsInN1YnNjcmlwdGlvbl9pZCI6ImM2ODNkNmNkLWRmNjktNDBhYS1iMjY4LTU4ZTcyMzdlMzIyNSIsImFuZHJvaWRfaWQiOiIwN2ZmMDBlNC1jMWE1LTQ2ODMtOWZjYi02MTNhNzM0ZDhkM2YiLCJhY2NvdW50X2lkIjoiYmUzNzIxNjItYzBhMC00OTAzLWE5ZTEtYTBiMzcyYmIxZGU5In0.xVyEIES9mZlwDIbWQYkIrpZ2viNSfI_bgRZ4pQjqBA4")
 		return nil
---
+++ test/integration/test-sql/existing-usage-report.sql
@@ -0,0 +1,2 @@
+INSERT INTO subscription(id, account_id, state) VALUES ('c015ce36-76df-4f3f-9352-5daea102d150', 'be372162-c0a0-4903-a9e1-a0b372bb1de9', 2);
+INSERT INTO usage_report(id, subscription_id, year, month) VALUES ('7a0e62ee-2685-4220-b3b6-3dd0e0c059b9', 'c015ce36-76df-4f3f-9352-5daea102d150', 2022, 6);
